9b9aefb934337657d5af2a68d37735eb77f1cfa9
adityackr <aditya09me@gmail.com>
2022-12-16 19:23:21 +0600

    feat: add button and modal

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,20 +1,45 @@
-import { Stack } from '@mui/material';
+import { Button, Container, Stack } from '@mui/material';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import * as React from 'react';
+import { useState } from 'react';
+import PlaylistForm from '../playlist-form';
 
 const NavBar = () => {
+	const [open, setOpen] = useState(false);
+
+	const handleClickOpen = () => {
+		setOpen(true);
+	};
+
+	const handleClose = () => {
+		setOpen(false);
+	};
+
+	const getPlaylistId = (playlistId) => {
+		console.log(playlistId);
+	};
+
 	return (
 		<Box sx={{ flexGrow: 1 }}>
-			<AppBar position="static">
-				<Toolbar>
-					<Stack spacing={1}>
-						<Typography variant="h3">Clean Youtube</Typography>
-						<Typography variant="body1">By Stack Learner</Typography>
-					</Stack>
-				</Toolbar>
+			<AppBar position="fixed" color="default" sx={{ py: 2 }}>
+				<Container maxWidth="lg">
+					<Toolbar>
+						<Stack sx={{ flexGrow: 1 }}>
+							<Typography variant="h4">Clean Youtube</Typography>
+							<Typography variant="body1">By Stack Learner</Typography>
+						</Stack>
+						<Button variant="contained" onClick={handleClickOpen}>
+							Add Playlist
+						</Button>
+						<PlaylistForm
+							open={open}
+							handleClose={handleClose}
+							getPlaylistId={getPlaylistId}
+						/>
+					</Toolbar>
+				</Container>
 			</AppBar>
 		</Box>
 	);
