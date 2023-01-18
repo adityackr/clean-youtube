543123ff8df28828404cadd9d091419beb8a0b04
adityackr <aditya09me@gmail.com>
2023-01-18 12:00:29 +0600

    feat: add api logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
-import React from 'react';
+import { useEffect } from 'react';
+import getPlaylist from './api';
 
 const App = () => {
+	useEffect(() => {
+		async function fetchData() {
+			const response = await getPlaylist('PL_XxuZqN0xVD0op-QDEgyXFA4fRPChvkl');
+			console.log(response);
+		}
+		fetchData();
+	}, []);
 	return <div>App</div>;
 };
 
---
+++ src/api/index.js
@@ -0,0 +1,57 @@
+import axios from 'axios';
+
+const key = import.meta.env.VITE_API_KEY;
+
+const getPlaylistItems = async (playlistId, pageToken = '', result = []) => {
+	const URL = `https://youtube.googleapis.com/youtube/v3/playlistItems?part=id%2CcontentDetails%2Csnippet&maxResults=20&playlistId=${playlistId}&key=${key}&pageToken=${pageToken}`;
+
+	const { data } = await axios.get(URL);
+	result = [...result, ...data.items];
+
+	if (data.nextPageToken) {
+		result = getPlaylistItems(playlistId, data.nextPageToken, result);
+	}
+
+	return result;
+};
+
+const getPlaylist = async (playlistId) => {
+	const URL = `https://youtube.googleapis.com/youtube/v3/playlists?part=snippet&id=${playlistId}&key=${key}`;
+
+	const { data } = await axios.get(URL);
+	let playlistItems = await getPlaylistItems(playlistId);
+
+	const {
+		title: playlistTitle,
+		description: playlistDescription,
+		thumbnails,
+		channelId,
+		channelTitle,
+	} = data?.items[0]?.snippet;
+
+	playlistItems = playlistItems.map((item) => {
+		const {
+			title,
+			description,
+			thumbnails: { medium },
+		} = item.snippet;
+		return {
+			title,
+			description,
+			thumbnail: medium,
+			contentDetails: item.contentDetails,
+		};
+	});
+
+	return {
+		playlistItems,
+		playlistId,
+		playlistTitle,
+		playlistDescription,
+		playlistThumbnails: thumbnails.default,
+		channelId,
+		channelTitle,
+	};
+};
+
+export default getPlaylist;
