99980169fd154efad0d5be346c7d6d31f7d58460
adityackr <aditya09me@gmail.com>
2023-01-21 14:24:06 +0600

    modify: form handled correctly

--- src/components/playlist-form/index.jsx
+++ src/components/playlist-form/index.jsx
@@ -5,18 +5,19 @@ import {
 	DialogContent,
 	DialogContentText,
 	DialogTitle,
-	TextField,
+	TextField
 } from '@mui/material';
 import { useStoreActions, useStoreState } from 'easy-peasy';
 import { useState } from 'react';
 
 const PlaylistForm = ({ open, handleClose }) => {
 	const playlist = useStoreActions((actions) => actions.playlist);
-	const { data, error } = useStoreState((state) => state.playlist);
+	const { data } = useStoreState((state) => state.playlist);
 
 	const [state, setState] = useState('');
 
-	const handleSubmit = () => {
+	const handleSubmit = (e) => {
+		e.preventDefault();
 		if (!state) {
 			alert('Please insert a valid id or link.');
 			return;
@@ -45,25 +46,27 @@ const PlaylistForm = ({ open, handleClose }) => {
 	return (
 		<Dialog open={open} onClose={handleClose}>
 			<DialogTitle>Add Playlist Id or Link</DialogTitle>
-			<DialogContent>
-				<DialogContentText>
-					Please insert a valid playlist id or link to add a new playlist. If
-					the id or link is not valid, playlist information can't be fetched.
-				</DialogContentText>
-				<TextField
-					autoFocus
-					margin="dense"
-					label="Playlist Id"
-					type="text"
-					fullWidth
-					variant="standard"
-					onChange={(e) => setState(e.target.value)}
-				/>
-			</DialogContent>
-			<DialogActions>
-				<Button onClick={handleClose}>Cancel</Button>
-				<Button onClick={handleSubmit}>Add Playlist</Button>
-			</DialogActions>
+			<form onSubmit={handleSubmit}>
+				<DialogContent>
+					<DialogContentText>
+						Please insert a valid playlist id or link to add a new playlist. If
+						the id or link is not valid, playlist information can't be fetched.
+					</DialogContentText>
+					<TextField
+						autoFocus
+						margin="dense"
+						label="Playlist Id"
+						type="text"
+						fullWidth
+						variant="standard"
+						onChange={(e) => setState(e.target.value)}
+					/>
+				</DialogContent>
+				<DialogActions>
+					<Button onClick={handleClose}>Cancel</Button>
+					<Button type="submit">Add Playlist</Button>
+				</DialogActions>
+			</form>
 		</Dialog>
 	);
 };
